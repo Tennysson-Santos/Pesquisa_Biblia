e1ff3e8c7e066949aa0f9e3bcc0fd9bbdb9980e0
Tennysson Santos <tennyssonsantos@gmail.com>
2021-01-24 10:13:59 -0300

    commit inicial

---
+++ crawler.py
@@ -0,0 +1,91 @@
+import requests
+
+import re
+
+from bs4 import BeautifulSoup
+
+import threading
+
+
+DOMINIO = 'https://django-anuncios.solyd.com.br'
+
+URL_AUTOMOVEIS = 'https://django-anuncios.solyd.com.br/automoveis/'
+
+TELEFONES = []
+
+
+def buscar(url):
+	try:
+		resposta = requests.get(url)
+		return resposta.text
+	except:
+		print('Houve algum erro!')
+		
+def parsing(resposta_url):
+	try:
+		soup = BeautifulSoup(resposta_url, 'html.parser')
+		return soup
+	except:
+		print('Ocorreu alum problema!')
+		
+def buscar_anuncio(soup):
+	cards_pai = soup.find('div', class_='ui three doubling link cards')
+	cards = cards_pai.find_all('a')
+	links = []
+	for card in cards:
+		link = card['href']
+		links.append(link)
+
+		
+	return links
+	
+def encontrar_anuncios(soup):
+	try:
+		anuncio = soup.find_all('div', class_='sixteen wide column')[2].p.get_text().strip()
+		print('Anuncios:',anuncio,'\n')
+	except:
+		print('Houve algum problema!')
+	
+	
+def mostrar_anuncios():
+	num = 0
+	for num in range(1,14):
+		resposta = buscar(URL_AUTOMOVEIS)
+		soup = parsing(resposta)
+		resposta = buscar_anuncio(soup)
+		anuncio = buscar(DOMINIO + resposta[num])
+		soup_anuncio = parsing(anuncio)
+		encontrar_anuncios(soup_anuncio)
+	
+	
+def encontrar_telefones(soup):
+	try:
+		anuncio = soup.find_all('div', class_='sixteen wide column')[2].p.get_text().strip()
+	except:
+		print('Houve algum problema!')			
+	regex = re.findall(r"\(?0?([1-9]{2})[ \-\.]{0,2}(9\d{4})[ \-\.]?(\d{4})", anuncio)
+	print('Possiveis números de telefone: ', regex)
+	
+def mostrar_telefones():
+	num = 0
+	for num in range(1,14):
+		resposta = buscar(URL_AUTOMOVEIS)
+		soup = parsing(resposta)
+		resposta = buscar_anuncio(soup)
+		anuncio = buscar(DOMINIO + resposta[num])
+		soup_anuncio = parsing(anuncio)
+		encontrar_telefones(soup_anuncio)
+		
+
+
+#mostrar_anuncios()
+
+mostrar_telefones()
+
+
+	
+
+
+
+
+
---
+++ pesquisa_Biblia.py
@@ -0,0 +1,61 @@
+import requests
+
+import re
+
+from bs4 import BeautifulSoup
+
+
+BIBLIA = []
+
+def buscar_url(url):
+	try:
+		resposta = requests.get(url)
+		return resposta.text
+	except:
+		print('Houve algum erro!')
+	
+def parsing(resposta_url):
+	try:
+		soup = BeautifulSoup(resposta_url, 'html.parser')
+		return soup
+	except:
+		print('Ocorreu alum problema!')
+		
+def buscar_livro(soup):
+	try:
+		livro = soup.find('div', class_='jss40')
+		livros = livro.find_all('p')
+		return livros
+	except:
+		print('Ocorreu algum problema!')
+
+def mostrar_livro():
+	try:
+		busca = buscar_url(URL)
+		soup = parsing(busca)
+		livro = buscar_livro(soup)
+		print(livro)	
+	except Exception as error:
+		print('Ocorreu algum problema')
+		print(error)
+		
+def salvar_livro():
+	busca = buscar_url(URL)
+	soup = parsing(busca)
+	livro = buscar_livro(soup)
+	BIBLIA.append(livro)
+	with open('biblia.text', 'w') as arquivo:
+		arquivo.write(f'{BIBLIA}')
+
+if __name__ == "__main__":
+	print('-----------------------------')
+	print('Consulta de livros da Biblia!')
+	print('-----------------------------')
+	print('Escolha um livro da biblia, Ex Matheus[mt]')
+	print('Escolha um Capitulo do livro, Ex [1]')
+
+livro = input('\nDigite um livro: ')
+cap = int(input('\nDigite um capítulo: '))
+URL = 'https://www.bibliaonline.com.br/naa/'+livro+'/'+str(cap)
+mostrar_livro()
+
