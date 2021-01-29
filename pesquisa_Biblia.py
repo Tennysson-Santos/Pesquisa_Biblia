0a25f7652dec26d74c4e40ba45458cac4e5c45bb
Tennysson Santos <tennyssonsantos@gmail.com>
2021-01-29 09:21:42 -0300

    novas alteracoes

--- pesquisa_Biblia.py
+++ pesquisa_Biblia.py
@@ -25,7 +25,7 @@ def buscar_livro(soup):
 	try:
 		livro = soup.find('div', class_='jss40')
 		versiculos = len(livro.find_all('p'))
-		print(f'Neste livro tem: {versiculos - 1} versículos!')
+		print(f'Este capítulo tem: {versiculos - 1} versículos!')
 		num = int(input('\nEscolha um versículo: '))
 		num -= 1
 		livros = livro.find_all('p')[num].get_text().strip()
@@ -42,7 +42,16 @@ def mostrar_livro():
 	except Exception as error:
 		print('Ocorreu algum problema')
 		print(error)
-		
+	
+def capitulo():
+	resposta = buscar_url('https://www.bibliaonline.com.br/naa/'+livro)
+ 
+	parsi = parsing(resposta)
+
+	capitulo = len(parsi.find('ul', class_='jss1'))
+
+	print(f'Este livro tem {capitulo} capítulos!')
+	
 def salvar_livro():
 	busca = buscar_url(URL)
 	soup = parsing(busca)
@@ -54,15 +63,15 @@ def salvar_livro():
 
 if __name__ == "__main__":
 	print('-----------------------------')
-	print('Consulta de livros da Biblia!')
+	print('      LEIA A BÍBLIA!'          )
 	print('-----------------------------')
-	print('Escolha um livro da biblia, Ex Matheus[mt]')
-	print('Escolha um Capitulo do livro, Ex [1]')
+	print('Escolha um livro da bíblia, Ex Matheus[mt]')
+
 
 livro = input('\nDigite um livro: ')
-cap = int(input('\nDigite um capítulo: '))
+capitulo()
+cap = int(input('\nEscolha um capítulo: '))
 URL = 'https://www.bibliaonline.com.br/naa/'+livro+'/'+str(cap)
-salvar_livro()
-#mostrar_livro()
-
+#salvar_livro()
+mostrar_livro()
 
