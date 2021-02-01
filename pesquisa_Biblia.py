a80ecb6cc9b8f4e0c0ecc7104eb2817836f4b27d
Tennysson Santos <tennyssonsantos@gmail.com>
2021-02-01 09:59:19 -0300

    finalizacao

--- pesquisa_Biblia.py
+++ pesquisa_Biblia.py
@@ -62,11 +62,26 @@ def salvar_livro():
 		arquivo.write(f'{BIBLIA}')
 
 if __name__ == "__main__":
-	print('-----------------------------')
-	print('      LEIA A BÍBLIA!'          )
-	print('-----------------------------')
-	print('Escolha um livro da bíblia, Ex Matheus[mt]')
-
+	print('\t                    ----------------------')
+	print(' \t                        LEIA A BÍBLIA!'          )
+	print('\t                    ----------------------')
+	print('\t                 Escolha um livro da bíblia!!\n')
+	print('''\t              *----* Velho Testamento *----*\n
+Gênesis[gn] - Êxodo[ex] - Levítico [lv] - Números[nm] - Deuteronômio[dt]\n
+Josué[js] - Juízes[jz] - Rute[rt] - 1 Samuel[1sm] - 2 Samuel[2sm] - 1 Reis[1rs]\n
+2 Reis[2rs] - 1 Crônicas[1cr] - 2 Crônicas[2cr] - Esdras[ed] - Neemias[ne]\n 
+Ester[et] - Jó[jó] - Salmos[sl] - Provérbios[pv] - Eclesiastes[ec]\n
+Cânticos[ct] - Isaías[is] - Jeremias[jr] - Lamentações[lm] - Ezequiel[ez]\n
+Daniel[dn] - Oséias[os] - Joel[jl] - Amós[am] - Obadias[ob] - Jonas[jn]\n
+Miquéias[mq] - Naum[na] - Habacuque[hc] - Sofonias[sf] - Ageu[ag] - Zacarias[zc] - Malaquias[ml]\n
+''')
+	print('''\t              *----* Novo Testamento *----\n*
+Mateus[mt] - Marcos[mc] - Lucas[lc] - João[jo] - Atos[atos] - Romanos[rm]\n
+1 Coríntios[1co] - 2 Coríntios[2co] - Gálatas[gl] - Efésios[ef] - Filipenses[fp]\n
+Colossenses[cl] - 1 Tessalonicenses[1ts] - 2 Tessalonicenses[2ts] - 1 Timóteo[1tm]\n
+2 Timóteo[2tm] - Tito[tt] - Filemom[fm] - Hebreus[hb] - Tiago[tg] - 1 Pedro[1pe]\n
+2 Pedro[2pe] - 1 João[1jo] - 2 João[2jo] - 3 João[3jo] - Judas[jd] - Apocalipse[ap]\n
+''')
 
 livro = input('\nDigite um livro: ')
 capitulo()
